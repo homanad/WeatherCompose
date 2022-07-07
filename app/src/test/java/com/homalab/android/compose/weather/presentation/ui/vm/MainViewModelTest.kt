f92bc27e8166d699069eda2cf8aa0ca75be7a164
Man Ho <hmman.ct@gmail.com>
2022-07-07 16:21:24 +0700

    Unit tests

--- app/src/test/java/com/homalab/android/compose/weather/presentation/ui/vm/MainViewModelTest.kt
+++ app/src/test/java/com/homalab/android/compose/weather/presentation/ui/vm/MainViewModelTest.kt
@@ -9,6 +9,7 @@ import com.homalab.android.compose.weather.domain.entity.WeatherData
 import com.homalab.android.compose.weather.domain.entity.subEntity.*
 import com.homalab.android.compose.weather.domain.repository.WeatherRepository
 import com.homalab.android.compose.weather.domain.usecase.GetCurrentWeatherUseCase
+import com.homalab.android.compose.weather.domain.usecase.GetForecastDataUseCase
 import com.homalab.android.compose.weather.domain.usecase.GetLastWeatherUseCase
 import com.homalab.android.compose.weather.domain.usecase.GetSavedCitiesUseCase
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -22,13 +23,18 @@ class MainViewModelTest {
 
     private val standardTestDispatcher = StandardTestDispatcher()
 
+    private val weatherRepository = WeatherRepositoryImpl(
+        WeatherRemoteDataSourceImpl(),
+        WeatherLocalDataSourceImpl()
+    )
+
+    private val weatherRepositoryWithException = WeatherRepositoryImpl(
+        ExceptionRemoteDataSourceImpl(),
+        WeatherLocalDataSourceImpl()
+    )
+
     @Test
     fun shouldReturnRemoteWeather() = runTest(standardTestDispatcher) {
-        val weatherRepository: WeatherRepository = WeatherRepositoryImpl(
-            WeatherRemoteDataSourceImpl(),
-            WeatherLocalDataSourceImpl()
-        )
-
         val useCase = GetCurrentWeatherUseCase(weatherRepository)
         val weatherData =
             useCase.invoke(GetCurrentWeatherUseCase.GetCurrentWeatherParam(10, 10.0, 10.0))
@@ -37,12 +43,7 @@ class MainViewModelTest {
 
     @Test
     fun shouldReturnLocalWeather() = runTest(standardTestDispatcher) {
-        val weatherRepository: WeatherRepository = WeatherRepositoryImpl(
-            ExceptionRemoteDataSourceImpl(),
-            WeatherLocalDataSourceImpl()
-        )
-
-        val useCase = GetCurrentWeatherUseCase(weatherRepository)
+        val useCase = GetCurrentWeatherUseCase(weatherRepositoryWithException)
         val weatherData =
             useCase.invoke(GetCurrentWeatherUseCase.GetCurrentWeatherParam(10, 10.0, 10.0))
 
@@ -50,27 +51,38 @@ class MainViewModelTest {
     }
 
     @Test
-    fun shouldReturnLastWeather() = runTest(standardTestDispatcher) {
-        val weatherRepository: WeatherRepository = WeatherRepositoryImpl(
-            ExceptionRemoteDataSourceImpl(),
-            WeatherLocalDataSourceImpl()
-        )
-
+    fun shouldReturnLastWeatherFromRemote() = runTest(standardTestDispatcher) {
         val useCase = GetLastWeatherUseCase(weatherRepository)
+        val weatherData = useCase.invoke(0)
+        Assert.assertEquals(weatherData, getRemoteWeatherData(weatherData.coord.lat, weatherData.coord.lon))
+    }
+
+    @Test
+    fun shouldReturnLastWeatherFromLocal() = runTest(standardTestDispatcher) {
+        val useCase = GetLastWeatherUseCase(weatherRepositoryWithException)
         val weatherData = useCase.invoke(0)
         Assert.assertEquals(weatherData, getLocalWeatherData(0))
     }
 
     @Test
     fun shouldReturnSavedWeathers() = runTest(standardTestDispatcher) {
-        val weatherRepository: WeatherRepository = WeatherRepositoryImpl(
-            ExceptionRemoteDataSourceImpl(),
-            WeatherLocalDataSourceImpl()
-        )
-
         val useCase = GetSavedCitiesUseCase(weatherRepository)
         val data = useCase.invoke(0)
         Assert.assertEquals(data, listOf<WeatherData>())
+    }
+
+    @Test
+    fun shouldReturnRemoteForecastData() = runTest(standardTestDispatcher) {
+        val useCase = GetForecastDataUseCase(weatherRepository)
+        val data = useCase.invoke(GetForecastDataUseCase.GetForecastDataParam(10.0, 20.0))
+        Assert.assertEquals(data, getRemoteForecastData(10.0, 20.0))
+    }
+
+    @Test
+    fun shouldReturnLocalForecastData() = runTest(standardTestDispatcher) {
+        val useCase = GetForecastDataUseCase(weatherRepositoryWithException)
+        val data = useCase.invoke(GetForecastDataUseCase.GetForecastDataParam(10.0, 20.0))
+        Assert.assertEquals(data, getLocalForecastData(10.0, 20.0))
     }
 }
 
@@ -92,11 +104,11 @@ class WeatherLocalDataSourceImpl : WeatherLocalDataSource {
     }
 
     override suspend fun getForecastData(lat: Double, lon: Double): ForecastData {
-        TODO("Not yet implemented")
+        return getLocalForecastData(lat, lon)
     }
 
     override suspend fun saveForecastData(forecastData: ForecastData): Long {
-        TODO("Not yet implemented")
+        return 1
     }
 }
 
@@ -106,7 +118,7 @@ class WeatherRemoteDataSourceImpl : WeatherRemoteDataSource {
     }
 
     override suspend fun getForecastData(lat: Double, lon: Double): ForecastData {
-        TODO("Not yet implemented")
+        return getRemoteForecastData(lat, lon)
     }
 }
 
@@ -116,7 +128,7 @@ class ExceptionRemoteDataSourceImpl : WeatherRemoteDataSource {
     }
 
     override suspend fun getForecastData(lat: Double, lon: Double): ForecastData {
-        TODO("Not yet implemented")
+        throw Exception("Network unavailable")
     }
 }
 
@@ -151,5 +163,61 @@ private fun getRemoteWeatherData(lat: Double, lon: Double): WeatherData {
         id = 1000,
         name = "remote",
         cod = 1
+    )
+}
+
+private fun getRemoteForecastData(lat: Double, lon: Double): ForecastData {
+    return ForecastData(
+        cnt = 1,
+        list = listOf(
+            ForecastItem(
+                dt = 10L,
+                main = Main(10f, 10f, 10f, 10f, 10f, 10f),
+                weather = listOf(),
+                wind = Wind(10f, 360, 10f),
+                visibility = 0,
+                pop = 0f,
+                sys = ForecastSys("n"),
+                dt_txt = "dt_txt"
+            )
+        ),
+        city = ForecastCity(
+            1,
+            "remote",
+            Coord(lat, lon),
+            country = "",
+            population = 100,
+            timeZone = 100,
+            sunrise = 1L,
+            sunset = 1L
+        )
+    )
+}
+
+private fun getLocalForecastData(lat: Double, lon: Double): ForecastData {
+    return ForecastData(
+        cnt = 1,
+        list = listOf(
+            ForecastItem(
+                dt = 10L,
+                main = Main(10f, 10f, 10f, 10f, 10f, 10f),
+                weather = listOf(),
+                wind = Wind(10f, 360, 10f),
+                visibility = 0,
+                pop = 0f,
+                sys = ForecastSys("n"),
+                dt_txt = "dt_txt"
+            )
+        ),
+        city = ForecastCity(
+            1,
+            "local",
+            Coord(lat, lon),
+            country = "",
+            population = 100,
+            timeZone = 100,
+            sunrise = 1L,
+            sunset = 1L
+        )
     )
 }
