ac4bdfe9efcfd6188ffacfdee5c38d99def89102
Man Ho <hmman.ct@gmail.com>
2022-07-09 14:28:58 +0700

    Draw line chart

--- app/src/main/java/com/homalab/android/compose/weather/presentation/ui/detail/DetailDisplay.kt
+++ app/src/main/java/com/homalab/android/compose/weather/presentation/ui/detail/DetailDisplay.kt
@@ -8,12 +8,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import com.homalab.android.compose.weather.R
 import com.homalab.android.compose.weather.presentation.components.HorizontalDivider
+import com.homalab.android.compose.weather.presentation.components.LineChart
 import com.homalab.android.compose.weather.presentation.components.MessageText
 import com.homalab.android.compose.weather.presentation.components.SmallSpacer
 import com.homalab.android.compose.weather.presentation.mapper.ForecastDayData
+import com.homalab.android.compose.weather.presentation.mapper.ForecastDayItem
 import com.homalab.android.compose.weather.util.Dimension1
 import com.homalab.android.compose.weather.util.Dimension2
 
@@ -34,14 +37,14 @@ fun DetailDisplay(
 @Composable
 fun DetailInfo(forecastDayData: ForecastDayData, modifier: Modifier = Modifier) {
     Column(modifier = modifier) {
-        DataChart(title = "Temp", data = forecastDayData.items[0].list.map { it.main.temp })
+        DataChart(title = "Temp", data = forecastDayData.items[1])
     }
 }
 
 @Composable
 fun DataChart(
     title: String,
-    data: List<Float>
+    data: ForecastDayItem
 ) {
     Column {
         Row(
@@ -54,5 +57,20 @@ fun DataChart(
             SmallSpacer()
             HorizontalDivider(modifier = Modifier.padding(start = Dimension1))
         }
+
+        val chartData = data.list.map {
+            it.main
+        }
+//        val verticalAxis = chartData.map { it.temp }.toMutableList()
+//
+//        verticalAxis.add(0f)
+
+        LineChart(
+            chartData = listOf(-10f, 10f, 20f, 30f),
+            lineValues = { it },
+            verticalAxisValues = listOf(-25f, -15f, -5f, 5f, 15f, 25f, 35f, 45f, 55f),
+            strokeColor = Color.Black,
+            dotColor = Color.Red
+        )
     }
 }
