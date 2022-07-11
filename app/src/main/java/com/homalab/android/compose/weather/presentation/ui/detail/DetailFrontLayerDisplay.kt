8a4127587b1f0107fe4b059e7bb25aa6f8e62c6a
Man Ho <hmman.ct@gmail.com>
2022-07-11 17:23:58 +0700

    Font layer views

--- app/src/main/java/com/homalab/android/compose/weather/presentation/ui/detail/DetailFrontLayerDisplay.kt
+++ app/src/main/java/com/homalab/android/compose/weather/presentation/ui/detail/DetailFrontLayerDisplay.kt
@@ -1,18 +1,53 @@
 package com.homalab.android.compose.weather.presentation.ui.detail
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import coil.compose.rememberAsyncImagePainter
+import com.homalab.android.compose.weather.presentation.components.DefaultSpacer
 import com.homalab.android.compose.weather.presentation.mapper.ForecastDayItem
+import com.homalab.android.compose.weather.util.Constants
+import com.homalab.android.compose.weather.util.Dimension1
+import com.homalab.android.compose.weather.util.WeatherConditionImageSize
 
 @Composable
 fun DetailFrontLayerDisplay(
     forecastDayItem: ForecastDayItem?
 ) {
-    Surface(modifier = Modifier.fillMaxSize()) {
-        Text(modifier = Modifier.fillMaxSize(), text = forecastDayItem.toString())
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Text(modifier = Modifier.fillMaxWidth(), text = "This day", textAlign = TextAlign.Center)
 
+        DefaultSpacer()
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .horizontalScroll(rememberScrollState())
+        ) {
+            forecastDayItem?.list?.forEach {
+                Column(
+                    modifier = Modifier.padding(Dimension1),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = it.weather[0].main)
+                    Image(
+                        painter = rememberAsyncImagePainter(
+                            Constants.OPEN_WEATHER_ICON_URL_PATTERN.format(
+                                it.weather[0].icon
+                            )
+                        ),
+                        contentDescription = null,
+                        modifier = Modifier.size(WeatherConditionImageSize)
+                    )
+                }
+            }
+        }
     }
 }
