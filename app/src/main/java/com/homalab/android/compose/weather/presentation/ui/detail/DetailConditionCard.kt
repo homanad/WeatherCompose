8d250c6e9ca5e85a9874c118696b8653386c46e7
Man Ho <hmman.ct@gmail.com>
2022-07-12 17:00:58 +0700

    Detail contition card

---
+++ app/src/main/java/com/homalab/android/compose/weather/presentation/ui/detail/DetailConditionCard.kt
@@ -0,0 +1,58 @@
+package com.homalab.android.compose.weather.presentation.ui.detail
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import coil.compose.rememberAsyncImagePainter
+import com.homalab.android.compose.weather.domain.entity.subEntity.ForecastItem
+import com.homalab.android.compose.weather.presentation.components.DefaultSpacer
+import com.homalab.android.compose.weather.util.Constants
+import com.homalab.android.compose.weather.util.Dimension2
+import com.homalab.android.compose.weather.util.TimeFormatter
+import com.homalab.android.compose.weather.util.WeatherDayConditionImageSize
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailConditionCard(modifier: Modifier = Modifier, forecastItem: ForecastItem, timeZone: Int) {
+    Card(modifier = modifier) {
+        Column(
+            modifier = Modifier.padding(Dimension2),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = forecastItem.weather[0].main,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                text = forecastItem.weather[0].description,
+                style = MaterialTheme.typography.titleSmall
+            )
+            Image(
+                painter = rememberAsyncImagePainter(
+                    Constants.OPEN_WEATHER_ICON_URL_PATTERN.format(
+                        forecastItem.weather[0].icon
+                    )
+                ),
+                contentDescription = null,
+                modifier = Modifier.size(WeatherDayConditionImageSize)
+            )
+
+            Text(
+                text = Constants.C_DEGREE_PATTERN.format(forecastItem.main.feels_like),
+                style = MaterialTheme.typography.titleLarge
+            )
+
+            DefaultSpacer()
+
+            Text(text = TimeFormatter.formatChartTime(forecastItem.dt, timeZone))
+        }
+    }
+}
--- app/src/main/java/com/homalab/android/compose/weather/presentation/ui/detail/DetailFrontLayerDisplay.kt
+++ app/src/main/java/com/homalab/android/compose/weather/presentation/ui/detail/DetailFrontLayerDisplay.kt
@@ -1,63 +1,41 @@
 package com.homalab.android.compose.weather.presentation.ui.detail
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
-import coil.compose.rememberAsyncImagePainter
-import com.homalab.android.compose.weather.presentation.components.DefaultSpacer
 import com.homalab.android.compose.weather.presentation.mapper.ForecastDayItem
-import com.homalab.android.compose.weather.util.Constants
 import com.homalab.android.compose.weather.util.Dimension1
-import com.homalab.android.compose.weather.util.TimeFormatter
-import com.homalab.android.compose.weather.util.WeatherDayConditionImageSize
+import com.homalab.android.compose.weather.util.Dimension2
 
 @Composable
 fun DetailFrontLayerDisplay(
     forecastDayItem: ForecastDayItem?
 ) {
-//    Surface(modifier = Modifier.fillMaxSize()) {
     Column {
-        Text(
-            modifier = Modifier.fillMaxWidth(),
-            text = "This day",
-            textAlign = TextAlign.Center
-        )
+//        Text(
+//            modifier = Modifier.fillMaxWidth(),
+//            text = "This day",
+//            textAlign = TextAlign.Center
+//        )
 
-        DefaultSpacer()
+//        SmallSpacer()
 
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .wrapContentHeight()
-                .horizontalScroll(rememberScrollState())
+                .padding(Dimension2)
+                .horizontalScroll(rememberScrollState()),
+            horizontalArrangement = Arrangement.Center
         ) {
             forecastDayItem?.list?.forEach {
-                Column(
-                    modifier = Modifier.padding(Dimension1),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(text = it.weather[0].main)
-                    Text(text = it.weather[0].description)
-                    Image(
-                        painter = rememberAsyncImagePainter(
-                            Constants.OPEN_WEATHER_ICON_URL_PATTERN.format(
-                                it.weather[0].icon
-                            )
-                        ),
-                        contentDescription = null,
-                        modifier = Modifier.size(WeatherDayConditionImageSize)
-                    )
-                    Text(text = TimeFormatter.formatChartTime(it.dt, 0))
-
-                }
+                DetailConditionCard(
+                    modifier = Modifier.padding(start = Dimension1, end = Dimension1),
+                    forecastItem = it,
+                    timeZone = forecastDayItem.timeZone
+                )
             }
         }
-//        }
     }
 }
