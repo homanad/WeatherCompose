23b5fb7747c9e9bfd69c9cacfae3a73225699bec
Man Ho <hmman.ct@gmail.com>
2022-07-14 10:26:43 +0700

    Fix front layer scrollable

--- app/src/main/java/com/homalab/android/compose/weather/presentation/ui/detail/DetailScreen.kt
+++ app/src/main/java/com/homalab/android/compose/weather/presentation/ui/detail/DetailScreen.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.BackdropScaffold
 import androidx.compose.material.BackdropValue
 import androidx.compose.material.ExperimentalMaterialApi
@@ -94,7 +96,10 @@ fun DetailScreen(
                         )
                     }
                 ) {
-                    DetailFrontLayerDisplay(forecastDayItem = detailState.forecastDayItem)
+                    DetailFrontLayerDisplay(
+                        forecastDayItem = detailState.forecastDayItem,
+                        modifier = Modifier.verticalScroll(rememberScrollState())
+                    )
                 }
             },
             stickyFrontLayer = true
